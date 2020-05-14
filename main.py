f91d3b1bb6ed1b31aadbcd736aa2dc7f8b4cdba2
Ubuntu <websiteuser@website.bgmxfxpb55nejgdhqhmoj4u4jc.gx.internal.cloudapp.net>
2020-05-14 06:56:53 +0000

    added support for no filenames

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import os
+from werkzeug.utils import secure_filename
 
 
 # app = Flask()
@@ -10,19 +11,23 @@ app = Flask(__name__, static_folder='static')
 def index():
     return render_template('index.html')
 
-app.config["UPLOAD_FOLDER"] = "A:/beam/uploads"
-#app.config["UPLOAD_FOLDER"] = "/home/websiteuser/beam/uploads"
+#app.config["UPLOAD_FOLDER"] = "A:/beam/uploads"
+app.config["UPLOAD_FOLDER"] = "/home/websiteuser/beam/uploads"
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST':
+    if request.method == "POST":
         if request.files:
-            f = request.files['image']
-            f.save(os.path.join(app.config["UPLOAD_FOLDER"], f.filename))
+            f = request.files["image"]
+            if f.filename == "":
+                print("No Filename")
+                return redirect(request.url)
+            filename = secure_filename(f.filename)
+            f.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            print("File saved")
             return redirect(request.url)
-            print('File has been successfully uploaded')
     return render_template('upload.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    app.run(host='0.0.0.0', port='80', debug='True')
